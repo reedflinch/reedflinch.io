9236c3ae292a53b0b4694f4aff78eddd99759bc1
Steve Flinchbaugh <stflinch1@gmail.com>
2017-08-21 22:23:42 -0400

    adds infra, deploy files (#1)
    * adds infra, deploy files
    
    * mkdir content
    
    * lock down base image version
    
    * clean up comments in infra

---
+++ infra/variables.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_version = ">= 0.9.1"
+
+  backend "s3" {
+    bucket         = "terraform-remote-state-reedflinch-io"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = "true"
+    dynamodb_table = "terraform-remote-state-reedflinch-io"
+  }
+}
+
+variable "region" {
+  description = "AWS region to provision resources"
+  type        = "string"
+  default     = "us-east-1"
+}
+
+provider "aws" {
+  region                  = "${var.region}"
+  allowed_account_ids     = [143994185263]
+}
+
+variable "domain" {
+  description = "DNS domain for web S3 distribution - (ex. reedflinch.io)"
+  type        = "string"
+  default     = "reedflinch.io"
+}
+
+variable "bucket_name" {
+  description = "Name of web S3 bucket"
+  type        = "string"
+  default     = "reedflinch-io"
+}
+
+data "aws_acm_certificate" "web" {
+  domain   = "${var.domain}"
+  statuses = ["ISSUED", "PENDING_VALIDATION"]
+}
